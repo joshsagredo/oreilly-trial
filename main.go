3822974631ed18e0f0d27ab4086b0ac0dbabbcc9
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-02-10 00:43:17 +0300

    set headers as seperate function

--- main.go
+++ main.go
@@ -11,18 +11,27 @@ import (
 	"net/http"
 )
 
-func main() {
-	// for more usable domains, check https://temp-mail.org/
-	emailDomain := flag.String("emailDomain", "jentrix.com", "usable domain for creating trial " +
-		"account, it should be a valid domain")
-	length := flag.Int("length", 12, "length of the random generated username and password")
-	flag.Parse()
+var (
+	logger *zap.Logger
+	err error
+	emailDomain *string
+	length *int
+)
 
-	logger, err := zap.NewProduction()
+func init() {
+	logger, err = zap.NewProduction()
 	if err != nil {
 		panic(err)
 	}
 
+	// for more usable domains, check https://temp-mail.org/
+	emailDomain = flag.String("emailDomain", "jentrix.com", "usable domain for creating trial " +
+		"account, it should be a valid domain")
+	length = flag.Int("length", 12, "length of the random generated username and password")
+	flag.Parse()
+}
+
+func main() {
 	createUserUrl := "https://learning.oreilly.com/api/v1/user/"
 	username := generateUsername(*length)
 	password := generatePassword(*length)
@@ -51,18 +60,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	req.Header.Set("authority", "learning.oreilly.com")
-	req.Header.Set("pragma", "no-cache")
-	req.Header.Set("cache-control", "no-cache")
-	req.Header.Set("accept", "application/json")
-	req.Header.Set("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36")
-	req.Header.Set("content-type", "application/json")
-	req.Header.Set("origin", "https://learning.oreilly.com")
-	req.Header.Set("sec-fetch-site", "same-origin")
-	req.Header.Set("sec-fetch-mode", "cors")
-	req.Header.Set("sec-fetch-dest", "empty")
-	req.Header.Set("referer", "https://learning.oreilly.com/p/register/")
-	req.Header.Set("accept-language", "en-US,en;q=0.9")
+
+	setHeaders(req)
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
--- utils.go
+++ utils.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"math/rand"
+	"net/http"
 	"strings"
 	"time"
 )
@@ -39,4 +40,19 @@ func generatePassword(length int) string {
 	})
 
 	return string(buf)
+}
+
+func setHeaders(req *http.Request) {
+	req.Header.Set("authority", "learning.oreilly.com")
+	req.Header.Set("pragma", "no-cache")
+	req.Header.Set("cache-control", "no-cache")
+	req.Header.Set("accept", "application/json")
+	req.Header.Set("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36")
+	req.Header.Set("content-type", "application/json")
+	req.Header.Set("origin", "https://learning.oreilly.com")
+	req.Header.Set("sec-fetch-site", "same-origin")
+	req.Header.Set("sec-fetch-mode", "cors")
+	req.Header.Set("sec-fetch-dest", "empty")
+	req.Header.Set("referer", "https://learning.oreilly.com/p/register/")
+	req.Header.Set("accept-language", "en-US,en;q=0.9")
 }
