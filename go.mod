165542823afb96215c3de5ea9bc911fbe96831c3
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-09-23 02:46:24 +0300

    configure zap

--- go.mod
+++ go.mod
@@ -5,11 +5,10 @@ go 1.16
 require (
 	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be // indirect
 	github.com/dimiro1/banner v1.1.0
-	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/spf13/pflag v1.0.5
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
-	go.uber.org/zap v1.19.0
-	golang.org/x/sys v0.0.0-20210820121016-41cdb8703e55 // indirect
-	golang.org/x/tools v0.1.5 // indirect
+	go.uber.org/zap v1.19.1
+	golang.org/x/sys v0.0.0-20210921065528-437939a70204 // indirect
 )
--- internal/logging/logging.go
+++ internal/logging/logging.go
@@ -1,17 +1,27 @@
 package logging
 
-import "go.uber.org/zap"
-
-var (
-	logger *zap.Logger
-	err    error
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
 )
 
+var logger *zap.Logger
+
 func init() {
-	logger, err = zap.NewProduction()
-	if err != nil {
-		panic(err)
+	cfgConsole := zapcore.EncoderConfig{
+		MessageKey:   "message",
+		LevelKey:     "severity",
+		EncodeLevel:  zapcore.LowercaseLevelEncoder,
+		TimeKey:      "time",
+		EncodeTime:   zapcore.RFC3339TimeEncoder,
+		CallerKey:    "caller",
+		EncodeCaller: zapcore.FullCallerEncoder,
 	}
+
+	core := zapcore.NewTee(zapcore.NewCore(zapcore.NewJSONEncoder(cfgConsole), zapcore.Lock(os.Stdout), zap.InfoLevel))
+	logger = zap.New(core)
+	logger.Info("An info level message")
 }
 
 // GetLogger returns the shared *zap.Logger
