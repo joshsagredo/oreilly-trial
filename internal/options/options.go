9b00256cafd29d21da641676db6ed5a29a76dbb4
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-11-22 16:13:37 +0300

    fix goreport warnings

--- internal/options/options.go
+++ internal/options/options.go
@@ -20,7 +20,7 @@ func GetOreillyTrialOptions() *OreillyTrialOptions {
 type OreillyTrialOptions struct {
 	// CreateUserUrl is the url of the user creation on Oreilly API
 	CreateUserUrl string
-	// EmailDomains is the comma seperated list of usable domain for creating trial account, it should be a valid domain
+	// EmailDomains is the comma separated list of usable domain for creating trial account, it should be a valid domain
 	EmailDomains []string
 	// RandomLength is the length of the random generated username and password
 	RandomLength int
@@ -32,6 +32,6 @@ func (oto *OreillyTrialOptions) addFlags(fs *pflag.FlagSet) {
 		"url of the user creation on Oreilly API")
 	// for more usable dummy domains, check https://temp-mail.org/
 	fs.StringSliceVar(&oto.EmailDomains, "emailDomains", []string{"jentrix.com", "geekale.com", "64ge.com", "frnla.com"},
-		"comma seperated list of usable domain for creating trial account, it should be a valid domain")
+		"comma separated list of usable domain for creating trial account, it should be a valid domain")
 	fs.IntVar(&oto.RandomLength, "randomLength", 16, "length of the random generated username and password")
 }
