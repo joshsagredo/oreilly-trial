b25891a566d5808bc925df42f7313a7ce939935f
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-11-22 15:41:54 +0300

    handle one more error condition

--- internal/oreilly/oreilly_test.go
+++ internal/oreilly/oreilly_test.go
@@ -5,9 +5,24 @@ import (
 	"testing"
 )
 
-// TestGenerate function tests if Generate function running properly
-func TestGenerate(t *testing.T) {
-	url := "https://learning.oreilly.com/api/v1/registration/individual/"
+var url = "https://learning.oreilly.com/api/v1/registration/individual/"
+
+// TestGenerateBrokenEmail function tests if Generate function fails with broken arguments and returns desired error
+func TestGenerateBrokenEmail(t *testing.T) {
+	expectedError := "{\"email\": [\"Enter a valid email address.\"]}"
+	oto := options.OreillyTrialOptions{
+		CreateUserUrl: url,
+		EmailDomains:  []string{"hasan"},
+		RandomLength:  12,
+	}
+
+	if err := Generate(&oto); err != nil && err.Error() != expectedError {
+		t.Fatalf("expected error should be: %v, got: %v", expectedError, err.Error())
+	}
+}
+
+// TestGenerateValidArgs function tests if Generate function running properly with proper values
+func TestGenerateValidArgs(t *testing.T) {
 	cases := []struct {
 		caseName string
 		oto      options.OreillyTrialOptions
@@ -27,7 +42,7 @@ func TestGenerate(t *testing.T) {
 	for _, tc := range cases {
 		t.Run(tc.caseName, func(t *testing.T) {
 			if err := Generate(&tc.oto); err != nil {
-				t.Fatalf("an error occurred while creating trial user")
+				t.Fatalf("expected error should be: nil, got: %v", err.Error())
 			}
 
 			t.Logf("trial account successfully created!")
