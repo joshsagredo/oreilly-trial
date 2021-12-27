1421a193564d238fca072d45f9ae1e3a1f70f8d5
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-12-28 01:20:32 +0300

    remove unneccassary global variable def

--- cmd/oreilly-trial/main.go
+++ cmd/oreilly-trial/main.go
@@ -11,13 +11,7 @@ import (
 	"strings"
 )
 
-var (
-	logger *zap.Logger
-)
-
 func init() {
-	logger = logging.GetLogger()
-
 	bannerBytes, _ := ioutil.ReadFile("banner.txt")
 	banner.Init(os.Stdout, true, false, strings.NewReader(string(bannerBytes)))
 }
@@ -25,6 +19,6 @@ func init() {
 func main() {
 	opts := options.GetOreillyTrialOptions()
 	if err := oreilly.Generate(opts); err != nil {
-		logger.Fatal("an error occurred while generating user", zap.String("error", err.Error()))
+		logging.GetLogger().Fatal("an error occurred while generating user", zap.String("error", err.Error()))
 	}
 }
