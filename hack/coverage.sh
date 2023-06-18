f5f7d960176e7d2362712c8dd8905e537c1096e6
bilalcaliskan <bilalcaliskan@protonmail.com>
2023-06-18 19:43:25 +0300

    chore: minor improvements [skip ci]

--- hack/coverage.sh
+++ hack/coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 go test -tags "unit e2e integration" ./... -race -p 1 -coverprofile=coverage.txt -covermode=atomic -ldflags="-X github.com/bilalcaliskan/oreilly-trial/internal/mail.token=${API_TOKEN}"
-go tool cover -html=coverage.txt -o cover.html
-open cover.html
+go tool cover -html=all_coverage.txt -o all_cover.html
+open all_cover.html
